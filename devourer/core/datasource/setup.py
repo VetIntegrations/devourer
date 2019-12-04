0a9d26244ad4ebcf92a4d3f19652e56a11a3edf5
Vadym Zakovinko <vadym@zakovinko.com>
2019-12-04 11:11:35 +0200

    [GD-65] data source configuration per customer (w/o ability to run all data sources for one customer at once)

--- devourer/core/datasource/setup.py
+++ devourer/core/datasource/setup.py
@@ -1,4 +1,5 @@
 import abc
+import functools
 import os
 import typing
 from aiohttp import web
@@ -14,5 +15,14 @@ class DataSourceSetupAbstract(abc.ABC):
     def __call__(self):
         ...
 
-    def to_route_view(self, name: str, view: typing.Type[web.View]) -> web.RouteDef:
-        return web.view(os.path.join(self.url_prefix, name), view)
+    def to_route_view(
+        self,
+        name: str,
+        view: typing.Type[web.View],
+        customer_name: str,
+        options: dict
+    ) -> web.RouteDef:
+        return web.view(
+            os.path.join(self.url_prefix, name),
+            functools.partial(view, customer_name=customer_name, options=options)
+        )
--- devourer/tests/test_app_initialization.py
+++ devourer/tests/test_app_initialization.py
@@ -11,14 +11,24 @@ def test_init_datasource(monkeypatch):
             testdata['app'] = app
             testdata['url_prefix'] = url_prefix
 
-        def __call__(self):
+        def __call__(self, customer_name, options):
             testdata['called'] = True
 
-    monkeypatch.setattr(config, "DATA_SOURCES", (("test-source", "test_source"), ))
+    monkeypatch.setattr(
+        config,
+        'CUSTOMERS',
+        {
+            'test-customer': {
+                'datasources': {
+                    'test-source': 'test_source',
+                },
+            }
+        }
+    )
     monkeypatch.setattr(module_loading, "import_string", lambda path: TestDataSource)
 
-    from devourer.main import init_datasources
+    from devourer.main import init_customers
 
-    init_datasources(object(), "/api/v-test")
-    assert testdata["url_prefix"] == "/api/v-test/test-source"
+    init_customers(object(), "/api/v-test")
+    assert testdata["url_prefix"] == "/api/v-test/test-customer/test-source"
     assert testdata["called"]
